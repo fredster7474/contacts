7acda91c6d513fb6ea96bd9e42b87559f8509f23
Freddie Simons <freddie_simons@hotmail.co.uk>
2018-02-16 14:34:11 +0100

    use JSON

--- tacta.rb
+++ tacta.rb
@@ -1,12 +1,19 @@
 # ./tacta.rb
 system "clear"
-contacts = []
 
-contacts << { name: "Thomas Jefferson", phone: "+1 206 310 1369" , email: "tjeff@us.gov" }
-contacts << { name: "Charles Darwin"  , phone: "+44 20 7123 4567", email: "darles@evolve.org" }
-contacts << { name: "Nikola Tesla"    , phone: "+385 43 987 3355", email: "nik@inductlabs.com" }
-contacts << { name: "Genghis Khan"    , phone: "+976 2 194 2222" , email: "contact@empire.com" }
-contacts << { name: "Malcom X"        , phone: "+1 310 155 8822" , email: "x@theroost.org" }
+require 'json'
+
+def read_contacts
+  json = File.read("contacts.json")
+  array = JSON.parse(json, { symbolize_names: true })
+end
+
+def write_contacts(contacts)
+  File.open("contacts.json", "w") do |file|
+    json = JSON.pretty_generate(contacts)
+    file.write(json)
+  end
+end
 
 def index(contacts)
   puts "Welcome here are the contacts"
@@ -44,6 +51,8 @@ def action_new(contacts)
 
   contacts << contact
 
+  write_contacts(contacts)
+
   puts
   puts "New contact created:"
   puts
@@ -71,6 +80,8 @@ def action_delete(contacts)
 
   contacts.delete_at(id - 1)
 
+  write_contacts(contacts)
+
   puts
 end
 
@@ -88,10 +99,13 @@ def action_search(contacts)
 end
 
 loop do
+
+  contacts = read_contacts
+
   index(contacts)
 
   puts
-response = ask "Who would you like to see (n for new, d for delete, s for search, q to quit)? "
+    response = ask "Who would you like to see (n for new, d for delete, s for search, q to quit)? "
 
   break if response == "q"
 
