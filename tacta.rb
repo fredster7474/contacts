bfad87517f363b598dbfb0410297412da1c0d29f
Freddie Simons <freddie_simons@hotmail.co.uk>
2018-02-16 12:05:56 +0100

    allows contact selection

--- tacta.rb
+++ tacta.rb
@@ -10,3 +10,16 @@ contacts << { name: "Malcom X"        , phone: "+1 310 155 8822" , email: "x@the
 contacts.each_with_index do |contact, index|
   puts "#{index + 1}) #{contact[:name]}"
 end
+
+puts
+print "Who would you like to see? "
+response = gets.chomp
+
+id = response.to_i
+
+contact = contacts[id - 1]
+
+puts
+puts contact[:name].to_s
+puts "phone: #{contact[:phone]}"
+puts "email: #{contact[:email]}"
